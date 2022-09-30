4a2924e1d9d42ae277d6779472b65a210aa3b023
Gabdullin_Roman <romaga59@gmail.com>
2022-09-30 22:14:47 +0500

    Добавил gitignore и решил 10 задачу

---
+++ Task_010/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write ("Введите трехзначное целое число:");
+int num_3th = Convert.ToInt32 (Console.ReadLine());
+int second = num_3th / 10 % 10;
+Console.Write ($"вторая цифра числа: {second}");
