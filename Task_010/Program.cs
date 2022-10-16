129cc33ed08310dd5b2beb198b2ee988a606d73f
Gabdullin_Roman <romaga59@gmail.com>
2022-10-16 22:29:54 +0500

    Сделал решение 10 задачи с помощью метода

--- Task_010/Program.cs
+++ Task_010/Program.cs
@@ -1,4 +1,19 @@
-﻿Console.Write ("Введите трехзначное целое число:");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write ("Введите трехзначное целое число: ");
 int num_3th = Convert.ToInt32 (Console.ReadLine());
-int second = num_3th / 10 % 10;
-Console.Write ($"вторая цифра числа: {second}");
+string secnum (int num)
+{
+    if (num >= 100 && num <=999)
+    {
+    int sm = num / 10 % 10;
+    return $"Вторая цифра числа {num} = {sm}";
+    }
+    else
+    {return "Ошибка ввода - число не трехзначное";}
+}
+Console.Write (secnum(num_3th));
