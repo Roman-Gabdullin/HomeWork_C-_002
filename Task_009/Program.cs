84a973ab86e0a1571da9e3647e7863f48e38b7c1
Gabdullin_Roman <romaga59@gmail.com>
2022-10-16 22:13:43 +0500

    Решил 9 задачу

---
+++ Task_009/Program.cs
@@ -0,0 +1,18 @@
+﻿// Дополнительная Задача 9
+// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string multiplicity (int n, int m1, int m2)
+{
+    if (n % m1 ==0 && n % m2 ==0)
+    {return $"Число {n} кратно {m1} и {m2}";}
+    else
+    {return $"Число {n} не кратно {m1} и {m2}";}
+}
+int mp1 =7;
+int mp2 = 23;
+Console.Write(multiplicity(num, mp1, mp2));
